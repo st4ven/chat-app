3b54f01e219f75b376150a4867775389ab8139c8
Steven <57573122+st4ven@users.noreply.github.com>
2024-11-02 15:56:16 -0700

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from 'cookie-parser';
@@ -9,6 +10,7 @@ import userRoutes from "./routes/user_routes.js";
 import connectToMongoDB from "./db/mongodb.js";
 import { app, server } from './socket/socket.js';
 
+const __dirname = path.resolve();
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
@@ -16,6 +18,7 @@ const PORT = process.env.PORT || 5000;
 // parse the incoming requests
 app.use(express.json());
 app.use(cookieParser());
+
 // get the signin, login, and logout endpoints
 app.use("/api/auth", authRoutes);
 
@@ -24,6 +27,12 @@ app.use("/api/messages", messageRoutes);
 
 // get the users endpoint
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+})
 
 // connect to the server and database
 server.listen(PORT, () => {
